ea15b93b4bbeeda7027994d2e30f5552613a623c
Dashko Ilya <ilya.dashko228@gmail.com>
2023-10-11 14:45:35 +0300

    Rename file

---
+++ Server/src/main/java/com/example/foodbook/FoodBookApplication.java
@@ -0,0 +1,13 @@
+package com.example.foodbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodBookApplication.class, args);
+    }
+
+}
