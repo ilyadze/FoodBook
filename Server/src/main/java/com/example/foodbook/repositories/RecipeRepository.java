3981d0fc96376287d73b8a45a2e8c81c85798889
Dashko Ilya <ilya.dashko228@gmail.com>
2023-10-11 15:34:54 +0300

    Add repositories(Equipment, Ingredient, Recipe) and Spring Data Rest dependency

---
+++ Server/src/main/java/com/example/foodbook/repositories/RecipeRepository.java
@@ -0,0 +1,9 @@
+package com.example.foodbook.repositories;
+
+import com.example.foodbook.models.Recipe;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+}
