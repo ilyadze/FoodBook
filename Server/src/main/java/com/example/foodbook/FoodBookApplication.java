3bde5acb1109b7ebd24d9dfd059ec6638d400871
vidu40k <90723662+vidu40k@users.noreply.github.com>
2023-10-12 23:44:42 +0300

    add ModelMapper

--- Server/src/main/java/com/example/foodbook/FoodBookApplication.java
+++ Server/src/main/java/com/example/foodbook/FoodBookApplication.java
@@ -1,7 +1,9 @@
 package com.example.foodbook;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FoodBookApplication {
@@ -10,4 +12,8 @@ public class FoodBookApplication {
         SpringApplication.run(FoodBookApplication.class, args);
     }
 
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
 }
