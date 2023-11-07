62ebc5f84dae3ad7b7c4d146c627271d011dbc87
vidu40k <90723662+vidu40k@users.noreply.github.com>
2023-11-07 19:49:40 +0300

    update LikeController

--- Server/src/main/java/com/example/foodbook/repositories/PostLikeRepository.java
+++ Server/src/main/java/com/example/foodbook/repositories/PostLikeRepository.java
@@ -8,6 +8,6 @@ import java.util.Optional;
 
 public interface PostLikeRepository extends JpaRepository<PostLike,Long> {
     Optional<List<PostLike>> findPostLikeByPostId(long postId);
-
+    Optional<List<PostLike>> findPostLikeByPersonUsername(String username);
     Integer countAllByPostId(long postId);
 }
